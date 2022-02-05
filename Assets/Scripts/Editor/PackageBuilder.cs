2de3b649ab6b41e3fd4f70fbf79a89a24185507d
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2022-02-05 12:33:38 +0700

    Build .tgz file from command line

---
+++ Assets/Scripts/Editor/PackageBuilder.cs
@@ -0,0 +1,33 @@
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public static class PackageBuilder
+{
+    private static void PackTgz()
+    {
+        const string sourceFolder = "Assets/PlayerPrefsWrapper";
+        const string targetFolder = "Builds";
+
+        var packRequest = Client.Pack(sourceFolder, targetFolder);
+
+        while (!packRequest.IsCompleted)
+        {
+        }
+
+        if (packRequest.Status == StatusCode.Failure)
+        {
+            var error = packRequest.Error;
+
+            Debug.LogError($"GZip tarball creation failed.");
+            Debug.LogError($"Error code: {error.errorCode}");
+            Debug.LogError($"Error message: {error.message}");
+        }
+        else if (packRequest.Status == StatusCode.Success)
+        {
+            var tarballPath = packRequest.Result.tarballPath;
+
+            Debug.Log($"GZip tarball created successfully.");
+            Debug.Log($"{tarballPath}");
+        }
+    }
+}
